d040e9a44a2bd37b0079bf723e74cbc5dbb5329a
okutomo takamitu <okutomotakamitsu@gmail.com>
2019-07-30 11:40:13 +0900

    2019/07/30大規模改変後

--- globalHeader.php
+++ globalHeader.php
@@ -5,40 +5,42 @@ header("Content-Type: text/html;charset=utf-8");
 	$urll = $_SERVER["REQUEST_URI"];
 	$domain = "https://www.saien.co.jp/";
 
-
 print <<<EOM
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "https://www.w3.org/TR/html4/loose.dtd">
-<html lang="ja" xml:lang="ja" xmlns="https://www.w3.org/1999/xhtml">
+
+
+
+<!DOCTYPE html>
+<html lang="ja">
 <head>
-	<meta https-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta https-equiv="content-language" content="ja" />
-	<title>Saienbé‹Ê‰€Œ|sêŒö®ƒTƒCƒg - <MTGetVar name="page_title"></title>
+<meta charset="UTF-8">
+<title>Saienbé‹Ê‰€Œ|sêŒö®ƒTƒCƒg</title>
+<meta name="keywords" content="é‹Ê,‰€Œ|,sê,<MTGetVar name="page_title">" />
+<meta name="description" content="Š”®‰ïĞ é‹Ê‰€Œ|sê - <MTGetVar name="page_title">" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="mixi-check-robots" content="noimage" />
+<meta property="og:title" content="Saienbé‹Ê‰€Œ|sêŒö®ƒTƒCƒg">
+<meta property="og:description" content="ué‹Ê‰€Œ|sêv‚ÌŒö®ƒEƒFƒuƒTƒCƒgBŠ”®‰ïĞé‹Ê‰€Œ|sê‚ÌŠé‹Æî•ñŒfÚB">
+<meta property="og:type" content="website">
+<meta property="og:url" content="/index.html">
+<meta property="og:image" content="">
+<meta property="og:locale" content="ja_JP">
+<meta name="p:domain_verify" content="f7368a4426544b684cac8f8fe249fc22"/>
+<meta name="viewport" content="width=1200" />
 
-<meta https-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta https-equiv="content-Style-type" content="text/css" />
-	<meta https-equiv="content-Script-type" content="text/javascript" />
-	<meta name="title" content="Š”®‰ïĞ é‹Ê‰€Œ|sê - <MTGetVar name="page_title">" />
-	<meta name="robots" content="ALL" />
-	<meta name="keywords" content="é‹Ê,‰€Œ|,sê,<MTGetVar name="page_title">" />
-	<meta name="description" content="Š”®‰ïĞ é‹Ê‰€Œ|sê - <MTGetVar name="page_title">" />
-	<meta name="author" content="SaitamaEngei Ichiba" />
-	<meta name="copyright" content="Copyright SaitamaEngei Ichiba Co ., Ltd All Right Reserve" />
-	<script src="<$MTBlogURL$>opt/include.js" type="text/javascript"></script>
 
+
+<!-- :::: highslide :::: -->
 <link href="<$MTBlogURL$>highslide/highslide.css" rel="stylesheet" type="text/css" />
-
 <script type="text/javascript" src="<$MTBlogURL$>highslide/highslide.js"></script>
 <script type="text/javascript"> hs.graphicsDir = '<$MTBlogURL$>highslide/graphics/';window.onload = function() {hs.preloadImages();}</script>
-<script type="text/javascript">
-hs.captionEval = 'this.thumb.alt';
-</script>
+<script type="text/javascript"> hs.captionEval = 'this.thumb.alt';</script>
 
-	<link href="<$MTBlogURL$>opt/import.css" rel="stylesheet" type="text/css" />
-	<link rel="<$MTBlogURL$>shortcut icon" href="<$MTBlogURL$>img/favicon.ico" />
-	<link rel="<$MTBlogURL$>INDEX" href="<$MTBlogURL$>index.html" />
-	<link rev="MADE" href="mailto:info@saien.co.jp" />
+<link href="<$MTBlogURL$>opt/import.css" rel="stylesheet" type="text/css" />
+<link rel="<$MTBlogURL$>shortcut icon" href="<$MTBlogURL$>img/favicon.ico" />
+<link rel="<$MTBlogURL$>INDEX" href="<$MTBlogURL$>index.html" />
+<link rev="MADE" href="mailto:info@saien.co.jp" />
 
+<!-- :::: facebook :::: -->
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
@@ -48,8 +50,7 @@ hs.captionEval = 'this.thumb.alt';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
-
-
+<!-- :::: ƒXƒ}[ƒgƒtƒHƒ“ƒTƒCƒg‚ÉˆÚ“® :::: -->
 <script type="text/javascript">
     var ua = navigator.userAgent.toUpperCase();
     if(document.referrer.indexOf(location.href.split('/')[2]) == -1 && (ua.indexOf('IPHONE') != -1 || (ua.indexOf('ANDROID') != -1 && ua.indexOf('MOBILE') != -1))) {
@@ -59,9 +60,10 @@ hs.captionEval = 'this.thumb.alt';
     }
 </script>
 
-
-
 </head>
+
+
+
 <body>
 
 <div id="wrap">
@@ -135,6 +137,10 @@ hs.captionEval = 'this.thumb.alt';
 		<li><a href="<$MTBlogURL$>sanchi.html"><span>Y’nî•ñ</a></li>
 	</ul>
 </div>
+
+
+
 EOM;
 
 ?> 
+
