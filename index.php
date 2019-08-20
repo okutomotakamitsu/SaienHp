0910a296302b4960e0b7c19856a11157938d559b
okutomo takamitu <okutomotakamitsu@gmail.com>
2019-08-20 09:44:57 +0900

    2019/08/20注目の園芸削除前

--- index.php
+++ index.php
@@ -2,75 +2,196 @@
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC4fOvzTfDIINRq6Or2mxTTnjigg_VJzI"></script>
 
-<!-- スライドショー -->
-  <link rel="stylesheet" type="text/css" href="https://saien.heteml.jp/okutomo/slick/slick.css"/>
-  <link rel="stylesheet" type="text/css" href="https://saien.heteml.jp/okutomo/slick/slick-theme2.css"/>
+<!-- :::: slick :::: -->
+  <link rel="stylesheet" type="text/css" href="okutomo/slick/slick.css"/>
+  <link rel="stylesheet" type="text/css" href="okutomo/slick/slick-theme.css"/>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://saien.heteml.jp/okutomo/slick/slick.min.js"></script>
+  <script src="okutomo/slick/slick.min.js"></script>
 
-<script type="text/javascript">  
+<script type="text/javascript">
 $(function() {
-  $('.slider').slick({
-    arrows: false,
-    asNavFor:'.slider-thumb',
-    autoplay: true    
-  });
-
- $('.slider-thumb').slick({
-    asNavFor:'.slider',
-    slidesToShow:4,
-  });
-
+	$('.slider').slick({
+arrows: true,
+		dots: true,
+		infinite: true,
+		autoplaySpeed: 3000,
+		speed: 800,
+		autoplay: true,
+		fade: true,
+		pauseOnHover: true,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
 });
 
+
 </script>
+
 		<div id="main">
 			<div id="top-main">
-				<div class="fl">
-
-
   <ul class="slider">
-     <li><img src="sp/SumahoGazou/tps_l_01.jpg" alt=""></li>
-     <li><img src="sp/SumahoGazou/tps_l_02.jpg" alt=""></li>
-     <li><img src="sp/SumahoGazou/tps_l_03.jpg" alt=""></li>
-     <li><img src="sp/SumahoGazou/tps_l_04.jpg" alt=""></li>
      <li><img src="sp/SumahoGazou/tps_l_05.jpg" alt=""></li>
-  </ul>
-
-  <ul class="slider-thumb">
-     <li><img src="sp/SumahoGazou/tps_l_01.jpg" alt=""></li>
-     <li><img src="sp/SumahoGazou/tps_l_02.jpg" alt=""></li>
      <li><img src="sp/SumahoGazou/tps_l_03.jpg" alt=""></li>
+     <li><img src="sp/SumahoGazou/tps_l_02.jpg" alt=""></li>
+     <li><img src="sp/SumahoGazou/tps_l_01.jpg" alt=""></li>
      <li><img src="sp/SumahoGazou/tps_l_04.jpg" alt=""></li>
-     <li><img src="sp/SumahoGazou/tps_l_05.jpg" alt=""></li>
   </ul>
-
-				</div>
 			</div>
-			<div class="column">
+  <div class="column">
+<div class="innerBlock">
+
+  <ul>
+    <li><p><img src="sp/SumahoGazou/inner01.jpg" alt="菊 精の一成"><br />菊 精の一成</p></li>
+    <li><p><img src="sp/SumahoGazou/inner02.jpg" alt="植木置場"><br />植木置場</p></li>
+    <li><p><img src="sp/SumahoGazou/inner03.jpg" alt="ソリダゴ"><br />ソリダゴ</p></li>
+    <li><p><img src="sp/SumahoGazou/inner04.jpg" alt="朝顔"><br />朝顔</p></li>
+    <li><p><img src="sp/SumahoGazou/inner05.jpg" alt="鬼灯"><br />鬼灯</p></li>
+    <li><p><img src="sp/SumahoGazou/inner06.jpg" alt="菊 精の曲"><br />菊 精の曲</p></li>
+    <li><p><img src="sp/SumahoGazou/inner07.jpg" alt="アスター"><br />アスター</p></li>
+    <li><p><img src="sp/SumahoGazou/inner08.jpg" alt="ブルーベリー"><br />ブルーベリー</p></li>
+    <li><p><img src="sp/SumahoGazou/inner09.jpg" alt="ハイビスカス"><br />ハイビスカス</p></li>
+  </ul>
+
+</div>
 				<div class="left-box">
 
 <$MTInclude module="top-headline"$>
 
 <section class="mainmargin">
-  <p class="font02">お知らせ</p>
+  <p class="font02"><q class="kazari3">お知らせ</q></p>
   <?php readfile("whatsnew/list_information.php"); ?>
   <div class="listCenter"><a href="whatsnew.html"><q>お知らせ一覧</q></a></div>
 </section>
 
+<!-- :::: Saien Art Flower :::: -->
+<section class="mainmargin">
+<p class="font02"><q class="kazari3">Saien Art Flower（オリジナル造花）</q></p>
+<ul class="toplist">
+  <li>
+  <p><img src="/sp/SumahoGazou/zouka01.jpg" width="210" height="210" alt="造花サンプル01"></p>
+  <p>造花サンプル01</p>
+  <p class="keisaibi">掲載日<span>2019.08.05</span></p>
+  </li>					
+  <li>
+  <p><img src="/sp/SumahoGazou/zouka02.jpg" width="210" height="210" alt="造花サンプル02"></p>
+  <p>造花サンプル02</p>
+  <p class="keisaibi">掲載日<span>2019.08.05</span></p>
+  </li>					
+  <li>
+  <p><img src="/sp/SumahoGazou/zouka03.jpg" width="210" height="210" alt="造花サンプル03"></p>
+  <p>造花サンプル03</p>
+  <p class="keisaibi">掲載日<span>2019.08.05</span></p>
+  </li>					
+
+  <li>
+  <p><img src="/sp/SumahoGazou/zouka04.jpg" width="210" height="210" alt="造花サンプル04"></p>
+  <p>造花サンプル04</p>
+  <p class="keisaibi">掲載日<span>2019.08.05</span></p>
+  </li>					
+  <li>
+  <p><img src="/sp/SumahoGazou/zouka05.jpg" width="210" height="210" alt="造花サンプル05"></p>
+  <p>造花サンプル05</p>
+  <p class="keisaibi">掲載日<span>2019.08.05</span></p>
+  </li>					
+  <li>
+  <p><img src="/sp/SumahoGazou/zouka06.jpg" width="210" height="210" alt="造花サンプル06"></p>
+  <p>造花サンプル06</p>
+  <p class="keisaibi">掲載日<span>2019.08.05</span></p>
+  </li>					
+</ul>
+</section>					
+
+<!-- :::: 生花卸部 :::: -->
+<section class="mainmargin">
+  <p class="font02"><q class="kazari3">生花卸部</q></p>
+<ul class="toplist">
+  <li>
+  <p><img src="/sp/SumahoGazou/seikaorosi01.jpg" width="210" height="210" alt="キングプロテア"></p>
+  <p>キングプロテア</p>
+  <p class="keisaibi">掲載日<span>2019.08.06</span></p>
+  </li>					
+  <li>
+  <p><img src="/sp/SumahoGazou/seikaorosi02.jpg" width="210" height="210" alt="ホオズキ"></p>
+  <p>ホオズキ</p>
+  <p class="keisaibi">掲載日<span>2019.08.06</span></p>
+  </li>					
+  <li>
+  <p><img src="/sp/SumahoGazou/seikaorosi03.jpg" width="210" height="210" alt="鑑賞用唐辛子"></p>
+  <p>鑑賞用唐辛子</p>
+  <p class="keisaibi">掲載日<span>2019.08.06</span></p>
+  </li>
+
+<li>
+  <p><img src="/sp/SumahoGazou/seikaorosi04.jpg" width="210" height="210" alt="赤カーネ"></p>
+  <p>赤カーネ</p>
+  <p class="keisaibi">掲載日<span>2019.08.06</span></p>
+  </li>
+<li>
+  <p><img src="/sp/SumahoGazou/seikaorosi05.jpg" width="210" height="210" alt="モンステラ"></p>
+  <p>モンステラ</p>
+  <p class="keisaibi">掲載日<span>2019.08.06</span></p>
+  </li>
+<li>
+  <p><img src="/sp/SumahoGazou/seikaorosi06.jpg" width="210" height="210" alt="ピンクッション"></p>
+  <p>ピンクッション</p>
+  <p class="keisaibi">掲載日<span>2019.08.06</span></p>
+  </li>					
+</ul>
+</section>
+
+<!-- :::: 注目の産地情報 :::: -->
 <section class="mainmargin">					
-  <p class="font02">注目の産地情報！</p>
+  <p class="font02"><q class="kazari3">注目の産地情報！</q></p>
   <div class="santi">
     <?php readfile("sanchi/list_sanchi.php"); ?>
   </div>
 </section>
 
+<!-- :::: 注目の資材情報 :::: -->
 <section class="mainmargin">
-  <p class="font02">注目の資材情報！</p>
+  <p class="font02"><q class="kazari3">注目の資材情報！</q></p>
   <div class="santi">
     <?php readfile("sizai/list_sizai.php"); ?>
   </div>
+</section>
+
+<!-- :::: 園芸ピックアップ :::: -->
+<section class="mainmargin">					
+  <p class="font02"><q class="kazari3">園芸ピックアップ</q></p>
+<ul class="toplist">
+  <li>
+  <p><img src="/sp/SumahoGazou/engeitop01.jpg" width="210" height="210" alt="黄花コスモス"></p>
+  <p>黄花コスモス</p>
+  <p class="keisaibi">掲載日<span>2019.08.20</span></p>
+  </li>					
+  <li>
+  <p><img src="/sp/SumahoGazou/engeitop02.jpg" width="210" height="210" alt="日々草・ミニナツ"></p>
+  <p>日々草・ミニナツ</p>
+  <p class="keisaibi">掲載日<span>2019.08.20</span></p>
+  </li>					
+  <li>
+  <p><img src="/sp/SumahoGazou/engeitop03.jpg" width="210" height="210" alt="リンドウ・岩手乙女"></p>
+  <p>リンドウ・岩手乙女</p>
+  <p class="keisaibi">掲載日<span>2019.08.20</span></p>
+  </li>					
+
+  <li>
+  <p><img src="/sp/SumahoGazou/engeitop04.jpg" width="210" height="210" alt="ブーゲンビレア"></p>
+  <p>ブーゲンビレア</p>
+  <p class="keisaibi">掲載日<span>2019.08.20</span></p>
+  </li>					
+  <li>
+  <p><img src="/sp/SumahoGazou/engeitop05.jpg" width="210" height="210" alt="ダイアンサス"></p>
+  <p>ダイアンサス</p>
+  <p class="keisaibi">掲載日<span>2019.08.20</span></p>
+  </li>					
+  <li>
+  <p><img src="/sp/SumahoGazou/engeitop06.jpg" width="210" height="210" alt="鑑賞用唐辛子・ツブツブの実"></p>
+  <p>鑑賞用唐辛子・ツブツブの実</p>
+  <p class="keisaibi">掲載日<span>2019.08.20</span></p>
+  </li>					
+</ul>
 </section>
 
 </div>
