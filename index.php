cd184391275f788915f0a9056c1158fa312d49d3
okutomo takamitu <okutomotakamitsu@gmail.com>
2019-08-27 06:32:41 +0900

    2019/08/27トップページ切替

--- index.php
+++ index.php
@@ -39,6 +39,11 @@ arrows: true,
   </ul>
 			</div>
   <div class="column">
+
+<div class="innerBlock2">
+<a data-pin-do = "embedBoard" data-pin-lang = "ja" data-pin-board-width = "1000" data-pin-scale-height = "120" data-pin-scale-width = "100" href = "https://www.pinterest.jp/018rflmujh8mxq2q4h0mr5rk9io6e0/%25E7%2594%259F%25E8%258A%25B1/"> </a>
+</div>
+
 <div class="innerBlock">
 
   <ul>
@@ -68,130 +73,159 @@ arrows: true,
 <section class="mainmargin">
 <p class="font02"><q class="kazari3">Saien Art Flower（オリジナル造花）</q></p>
 <ul class="toplist">
-  <li>
-  <p><img src="/sp/SumahoGazou/zouka01.jpg" width="210" height="210" alt="造花サンプル01"></p>
+  <li><p><a href="/sp/SumahoGazou/zouka01_L.jpg" data-lightbox="group_zouka" data-title="造花サンプル01"/><img src="/sp/SumahoGazou/zouka01.jpg" alt="造花サンプル01"></a></p>
   <p>造花サンプル01</p>
-  <p class="keisaibi">掲載日<span>2019.08.05</span></p>
-  </li>					
-  <li>
-  <p><img src="/sp/SumahoGazou/zouka02.jpg" width="210" height="210" alt="造花サンプル02"></p>
+  <p class="keisaibi">掲載日<span>2019.08.22</span></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/zouka02_L.jpg" data-lightbox="group_zouka" data-title="造花サンプル02"/><img src="/sp/SumahoGazou/zouka02.jpg" alt="造花サンプル02"></a></p>
   <p>造花サンプル02</p>
-  <p class="keisaibi">掲載日<span>2019.08.05</span></p>
-  </li>					
-  <li>
-  <p><img src="/sp/SumahoGazou/zouka03.jpg" width="210" height="210" alt="造花サンプル03"></p>
+  <p class="keisaibi">掲載日<span>2019.08.22</span></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/zouka03_L.jpg" data-lightbox="group_zouka" data-title="造花サンプル03"/><img src="/sp/SumahoGazou/zouka03.jpg" alt="造花サンプル03"></a></p>
   <p>造花サンプル03</p>
-  <p class="keisaibi">掲載日<span>2019.08.05</span></p>
-  </li>					
+  <p class="keisaibi">掲載日<span>2019.08.22</span></p>
+  </li>
 
-  <li>
-  <p><img src="/sp/SumahoGazou/zouka04.jpg" width="210" height="210" alt="造花サンプル04"></p>
+  <li><p><a href="/sp/SumahoGazou/zouka04_L.jpg" data-lightbox="group_zouka" data-title="造花サンプル04"/><img src="/sp/SumahoGazou/zouka04.jpg" alt="造花サンプル04"></a></p>
   <p>造花サンプル04</p>
-  <p class="keisaibi">掲載日<span>2019.08.05</span></p>
-  </li>					
-  <li>
-  <p><img src="/sp/SumahoGazou/zouka05.jpg" width="210" height="210" alt="造花サンプル05"></p>
-  <p>造花サンプル05</p>
-  <p class="keisaibi">掲載日<span>2019.08.05</span></p>
-  </li>					
-  <li>
-  <p><img src="/sp/SumahoGazou/zouka06.jpg" width="210" height="210" alt="造花サンプル06"></p>
+  <p class="keisaibi">掲載日<span>2019.08.22</span></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/zouka05_L.jpg" data-lightbox="group_zouka" data-title="造花サンプル05"/><img src="/sp/SumahoGazou/zouka05.jpg" alt="造花サンプル05"></a></p>
+  <p>造花サンプル03</p>
+  <p class="keisaibi">掲載日<span>2019.08.22</span></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/zouka06_L.jpg" data-lightbox="group_zouka" data-title="造花サンプル06"/><img src="/sp/SumahoGazou/zouka06.jpg" alt="造花サンプル06"></a></p>
   <p>造花サンプル06</p>
-  <p class="keisaibi">掲載日<span>2019.08.05</span></p>
-  </li>					
+  <p class="keisaibi">掲載日<span>2019.08.22</span></p>
+  </li>
 </ul>
+<div class="box_top3">
+<p>Saien Art Flower は究極のリアリティを追求し弊社の工場にて一点一点ハンドメイドで仕上げています。そのため実物に限りなく近い造形美そしてみずみずしい質感を再現したこだわりのアートフラワーです。</p>
+<P>お誕生日やご入学、ご結婚のお祝いや開店のお祝い、お見舞いや返礼品、会社や学校の受付、病院の待合い等々～あらゆるシチュエーションで華やぎと快適さを提供いたします。</p>
+<p>詳細・ご質問等はお電話にて、ご連絡お待ちしております。</p>
+<p class="align-r">TEL.0480-69-1125（造花部）</p>
+</div>
 </section>					
 
 <!-- :::: 生花卸部 :::: -->
 <section class="mainmargin">
   <p class="font02"><q class="kazari3">生花卸部</q></p>
 <ul class="toplist">
-  <li>
-  <p><img src="/sp/SumahoGazou/seikaorosi01.jpg" width="210" height="210" alt="キングプロテア"></p>
-  <p>キングプロテア</p>
-  <p class="keisaibi">掲載日<span>2019.08.06</span></p>
-  </li>					
-  <li>
-  <p><img src="/sp/SumahoGazou/seikaorosi02.jpg" width="210" height="210" alt="ホオズキ"></p>
-  <p>ホオズキ</p>
-  <p class="keisaibi">掲載日<span>2019.08.06</span></p>
-  </li>					
-  <li>
-  <p><img src="/sp/SumahoGazou/seikaorosi03.jpg" width="210" height="210" alt="鑑賞用唐辛子"></p>
-  <p>鑑賞用唐辛子</p>
-  <p class="keisaibi">掲載日<span>2019.08.06</span></p>
+  <li><p><a href="/sp/SumahoGazou/seikaorosi01_L.jpg" data-lightbox="group_seikaorosi" data-title="クラスペディア"/><img src="/sp/SumahoGazou/seikaorosi01.jpg" alt="クラスペディア"></a></p>
+  <p>クラスペディア</p>
+  <p class="keisaibi">掲載日<span>2019.08.21</span></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/seikaorosi02_L.jpg" data-lightbox="group_seikaorosi" data-title="セロシア・グリーンボール"/><img src="/sp/SumahoGazou/seikaorosi02.jpg" alt="セロシア・グリーンボール"></a></p>
+  <p>セロシア・グリーンボール</p>
+  <p class="keisaibi">掲載日<span>2019.08.21</span></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/seikaorosi03_L.jpg" data-lightbox="group_seikaorosi" data-title="ワックスフラワー"/><img src="/sp/SumahoGazou/seikaorosi03.jpg" alt="ワックスフラワー"></a></p>
+  <p>ワックスフラワー</p>
+  <p class="keisaibi">掲載日<span>2019.08.21</span></p>
   </li>
 
-<li>
-  <p><img src="/sp/SumahoGazou/seikaorosi04.jpg" width="210" height="210" alt="赤カーネ"></p>
-  <p>赤カーネ</p>
-  <p class="keisaibi">掲載日<span>2019.08.06</span></p>
+  <li><p><a href="/sp/SumahoGazou/seikaorosi04_L.jpg" data-lightbox="group_seikaorosi" data-title="SPカーネーション"/><img src="/sp/SumahoGazou/seikaorosi04.jpg" alt="SPカーネーション"></a></p>
+  <p>SPカーネーション</p>
+  <p class="keisaibi">掲載日<span>2019.08.21</span></p>
   </li>
-<li>
-  <p><img src="/sp/SumahoGazou/seikaorosi05.jpg" width="210" height="210" alt="モンステラ"></p>
-  <p>モンステラ</p>
-  <p class="keisaibi">掲載日<span>2019.08.06</span></p>
+  <li><p><a href="/sp/SumahoGazou/seikaorosi05_L.jpg" data-lightbox="group_seikaorosi" data-title="アスパラガス・ナナス"/><img src="/sp/SumahoGazou/seikaorosi05.jpg" alt="アスパラガス・ナナス"></a></p>
+  <p>アスパラガス・ナナス</p>
+  <p class="keisaibi">掲載日<span>2019.08.21</span></p>
   </li>
-<li>
-  <p><img src="/sp/SumahoGazou/seikaorosi06.jpg" width="210" height="210" alt="ピンクッション"></p>
-  <p>ピンクッション</p>
-  <p class="keisaibi">掲載日<span>2019.08.06</span></p>
-  </li>					
+  <li><p><a href="/sp/SumahoGazou/seikaorosi06_L.jpg" data-lightbox="group_seikaorosi" data-title="ポリシャス"/><img src="/sp/SumahoGazou/seikaorosi06.jpg" alt="ポリシャス"></a></p>
+  <p>ポリシャス</p>
+  <p class="keisaibi">掲載日<span>2019.08.21</span></p>
+  </li>
 </ul>
+<div class="box_top3">
+<P>フラワーデザイナー・生け花の先生・定期的に生花を必要とする店舗経営者様などの主に競売で扱うよりも小ロッドでのご購入を希望するお客様のご登録をお待ちしております。</p>
+<p>話は聞くんだけどなかなか声がかけにくい又は機会があれば一度見学してみたい方などまずは担当までお気軽にお問い合わせください。
+<p class="align-r">TEL.0480-69-1208（生花卸部）</p>
+</div>
 </section>
 
-<!-- :::: 注目の産地情報 :::: -->
+
+
+<!-- :::: lightbox2 :::: -->
+<link href="/lightbox2/lightbox.css" rel="stylesheet">
+<script src="/lightbox2/lightbox.min.js" type="text/javascript"></script>
+
+
+
+<!-- :::: 園芸部 :::: -->
 <section class="mainmargin">					
-  <p class="font02"><q class="kazari3">注目の産地情報！</q></p>
-  <div class="santi">
-    <?php readfile("sanchi/list_sanchi.php"); ?>
-  </div>
-</section>
-
-<!-- :::: 注目の資材情報 :::: -->
-<section class="mainmargin">
-  <p class="font02"><q class="kazari3">注目の資材情報！</q></p>
-  <div class="santi">
-    <?php readfile("sizai/list_sizai.php"); ?>
-  </div>
-</section>
-
-<!-- :::: 園芸ピックアップ :::: -->
-<section class="mainmargin">					
-  <p class="font02"><q class="kazari3">園芸ピックアップ</q></p>
+  <p class="font02"><q class="kazari3">園芸部</q></p>
 <ul class="toplist">
-  <li>
-  <p><img src="/sp/SumahoGazou/engeitop01.jpg" width="210" height="210" alt="黄花コスモス"></p>
+  <li><p><a href="/sp/SumahoGazou/engeitop01_L.jpg" data-lightbox="group_engeitop" data-title="サンプル画像01"/><img src="/sp/SumahoGazou/engeitop01.jpg" alt="黄花コスモス"></a></p>
   <p>黄花コスモス</p>
   <p class="keisaibi">掲載日<span>2019.08.20</span></p>
-  </li>					
-  <li>
-  <p><img src="/sp/SumahoGazou/engeitop02.jpg" width="210" height="210" alt="日々草・ミニナツ"></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/engeitop02_L.jpg" data-lightbox="group_engeitop" data-title="日々草・ミニナツ"/><img src="/sp/SumahoGazou/engeitop02.jpg" alt="日々草・ミニナツ"></a></p>
   <p>日々草・ミニナツ</p>
   <p class="keisaibi">掲載日<span>2019.08.20</span></p>
-  </li>					
-  <li>
-  <p><img src="/sp/SumahoGazou/engeitop03.jpg" width="210" height="210" alt="リンドウ・岩手乙女"></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/engeitop03_L.jpg" data-lightbox="group_engeitop" data-title="リンドウ・岩手乙女"/><img src="/sp/SumahoGazou/engeitop03.jpg" alt="リンドウ・岩手乙女"></a></p>
   <p>リンドウ・岩手乙女</p>
   <p class="keisaibi">掲載日<span>2019.08.20</span></p>
-  </li>					
+  </li>
 
-  <li>
-  <p><img src="/sp/SumahoGazou/engeitop04.jpg" width="210" height="210" alt="ブーゲンビレア"></p>
+  <li><p><a href="/sp/SumahoGazou/engeitop04_L.jpg" data-lightbox="group_engeitop" data-title="ブーゲンビレア"/><img src="/sp/SumahoGazou/engeitop04.jpg" alt="ブーゲンビレア"></a></p>
   <p>ブーゲンビレア</p>
   <p class="keisaibi">掲載日<span>2019.08.20</span></p>
-  </li>					
-  <li>
-  <p><img src="/sp/SumahoGazou/engeitop05.jpg" width="210" height="210" alt="ダイアンサス"></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/engeitop05_L.jpg" data-lightbox="group_engeitop" data-title="ダイアンサス"/><img src="/sp/SumahoGazou/engeitop05.jpg" alt="ダイアンサス"></a></p>
   <p>ダイアンサス</p>
   <p class="keisaibi">掲載日<span>2019.08.20</span></p>
-  </li>					
-  <li>
-  <p><img src="/sp/SumahoGazou/engeitop06.jpg" width="210" height="210" alt="鑑賞用唐辛子・ツブツブの実"></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/engeitop06_L.jpg" data-lightbox="group_engeitop" data-title="鑑賞用唐辛子・ツブツブの実"/><img src="/sp/SumahoGazou/engeitop06.jpg" alt="鑑賞用唐辛子・ツブツブの実"></a></p>
   <p>鑑賞用唐辛子・ツブツブの実</p>
   <p class="keisaibi">掲載日<span>2019.08.20</span></p>
-  </li>					
+  </li>
 </ul>
+<div class="box_top3">
+<p>苗物・鉢物・観葉植物・胡蝶蘭など季節に応じた様々な園芸を取り扱っております。</p>
+<P>生産者登録及び量販店様・小売店様などの市場登録は随時対応致します。</p>
+<p>また、一般のお客様は埼玉園芸市場杉戸卸部にて対応致します。</p>
+<p class="align-r">TEL.0480-69-1203（園芸部）</p>
+</div>
+</section>
+
+<!-- :::: 資材部 :::: -->
+<section class="mainmargin">					
+  <p class="font02"><q class="kazari3">資材部</q></p>
+<ul class="toplist">
+  <li><p><a href="/sp/SumahoGazou/sizaitop01_L.jpg" data-lightbox="group_sizaitop" data-title="鉢01"/><img src="/sp/SumahoGazou/sizaitop01.jpg" alt="鉢01"></a></p>
+  <p>鉢01</p>
+  <p class="keisaibi">掲載日<span>2019.08.20</span></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/sizaitop02_L.jpg" data-lightbox="group_sizaitop" data-title="鉢02"/><img src="/sp/SumahoGazou/sizaitop02.jpg" alt="鉢02"></a></p>
+  <p>鉢02</p>
+  <p class="keisaibi">掲載日<span>2019.08.20</span></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/sizaitop03_L.jpg" data-lightbox="group_sizaitop" data-title="鉢03"/><img src="/sp/SumahoGazou/sizaitop03.jpg" alt="鉢03"></a></p>
+  <p>鉢03</p>
+  <p class="keisaibi">掲載日<span>2019.08.20</span></p>
+  </li>
+
+  <li><p><a href="/sp/SumahoGazou/sizaitop04_L.jpg" data-lightbox="group_sizaitop" data-title="鉢04"/><img src="/sp/SumahoGazou/sizaitop04.jpg" alt="鉢04"></a></p>
+  <p>鉢04</p>
+  <p class="keisaibi">掲載日<span>2019.08.20</span></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/sizaitop05_L.jpg" data-lightbox="group_sizaitop" data-title="鉢05"/><img src="/sp/SumahoGazou/sizaitop05.jpg" alt="鉢05"></a></p>
+  <p>鉢05</p>
+  <p class="keisaibi">掲載日<span>2019.08.20</span></p>
+  </li>
+  <li><p><a href="/sp/SumahoGazou/sizaitop06_L.jpg" data-lightbox="group_sizaitop" data-title="鉢06"/><img src="/sp/SumahoGazou/sizaitop06.jpg" alt="鉢06"></a></p>
+  <p>鉢06</p>
+  <p class="keisaibi">掲載日<span>2019.08.20</span></p>
+  </li>
+</ul>
+<div class="box_top3">
+<p>多種多様の園芸資材を取り扱っております。</p>
+<p>特に造花資材・大小鉢物・ラッピング資材は充実しており自信があります。</p>
+<P>ここでしか手に入らない資材や掘り出し物も多数有ります。</p>
+<p class="align-r">TEL.0480-69-1208（生花卸部）</p>
+</div>
 </section>
 
 </div>
